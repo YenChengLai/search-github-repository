53fbeefb32b326763024242826a609906fd04e94
YenChengLai <frank810618@gmail.com>
2020-12-01 21:25:21 +0800

    minor adjusts and add search function

--- index.ts
+++ index.ts
@@ -5,19 +5,32 @@ import {
   debounceTime,
   distinctUntilChanged,
   map,
-  switchMap
+  switchMap,
+  filter
 } from "rxjs/operators";
 
-const keyword$ = fromEvent(document.getElementById("keyword"), "input").pipe(
-  map(event => (event.target as HTMLInputElement).value)
-);
+const keyword$ = fromEvent(document.getElementById("keyword"), "input");
 
 keyword$
   .pipe(
+    map(event => (event.target as HTMLInputElement).value),
     debounceTime(500),
     distinctUntilChanged(),
+    filter(keyword => keyword.length >= 3),
     switchMap(keyword => dataUtils.getSuggestions(keyword))
   )
   .subscribe(options => {
     domUtils.fillAutoSuggestions(options);
   });
+
+const search$ = fromEvent(document.getElementById("search"), "click");
+
+search$
+  .pipe(
+    switchMap(_ =>
+      dataUtils.getSearchResult(
+        (document.getElementById("keyword") as HTMLInputElement).value
+      )
+    )
+  )
+  .subscribe(result => domUtils.fillSearchResult(result));
