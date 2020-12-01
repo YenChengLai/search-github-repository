5a6bb87401a0895406a6fb0457b2123f18dca34c
YenChengLai <frank810618@gmail.com>
2020-12-01 12:38:08 +0800

    add keyword subscription

--- index.ts
+++ index.ts
@@ -1,2 +1,10 @@
-import * as domUtils from './dom-utils';
-import * as dataUtils from './data-utils';
+import * as domUtils from "./dom-utils";
+import * as dataUtils from "./data-utils";
+import { fromEvent } from "rxjs";
+import { map } from "rxjs/operators";
+
+const keyword$ = fromEvent(document.getElementById("keyword"), "input").pipe(
+  map(event => (event.target as HTMLInputElement).value)
+);
+
+keyword$.subscribe(console.log);
