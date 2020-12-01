745d10c5f2005c281e563277940124c55e909b31
YenChengLai <frank810618@gmail.com>
2020-12-01 21:14:34 +0800

    update keyword subscription

--- index.ts
+++ index.ts
@@ -1,14 +1,23 @@
 import * as domUtils from "./dom-utils";
 import * as dataUtils from "./data-utils";
 import { fromEvent } from "rxjs";
-import { map } from "rxjs/operators";
+import {
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  switchMap
+} from "rxjs/operators";
 
 const keyword$ = fromEvent(document.getElementById("keyword"), "input").pipe(
   map(event => (event.target as HTMLInputElement).value)
 );
 
-keyword$.subscribe(keyword => {
-  dataUtils.getSuggestions(keyword).subscribe(options => {
+keyword$
+  .pipe(
+    debounceTime(500),
+    distinctUntilChanged(),
+    switchMap(keyword => dataUtils.getSuggestions(keyword))
+  )
+  .subscribe(options => {
     domUtils.fillAutoSuggestions(options);
   });
-});
